0fb8fcdb21cbc867115dc63dbf8754ad12340303
muhhilmi <mailhilmi@gmail.com>
2024-03-10 09:42:41 +0700

    upload project

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,18 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+  static const HOME = _Paths.HOME;
+  static const ADD_MAHASISWA = _Paths.ADD_MAHASISWA;
+  static const DETAIL_MAHASISWA = _Paths.DETAIL_MAHASISWA;
+  static const UPDATE_MAHASISWA = _Paths.UPDATE_MAHASISWA;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const HOME = '/home';
+  static const ADD_MAHASISWA = '/add-mahasiswa';
+  static const DETAIL_MAHASISWA = '/detail-mahasiswa';
+  static const UPDATE_MAHASISWA = '/update-mahasiswa';
+}
